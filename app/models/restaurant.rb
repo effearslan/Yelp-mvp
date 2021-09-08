e16ac379b5a1f0add196540adc9fed6a8c8800c8
Efe Arslan <effecarslan@gmail.com>
2021-09-08 17:03:30 +0300

    corrected style

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   LIST = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   validates :name, presence: true
   validates :address, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [ :index, :show, :new, :create ] do
+  resources :restaurants, only: [ :index, :show, :new, :create, :destroy ] do
     resources :reviews, only: [ :new, :create ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
